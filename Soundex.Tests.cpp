031accfc89c1e0aedbb1d5121205f91b3e0d3e85
Rakshith08URK <rajrakshith08@gmail.com>
2024-07-05 13:32:51 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,3 +8,31 @@ void test_soundex_simple() {
     char* actual = soundex(input);
     assert(strcmp(actual, expected) == 0);
 }
+
+void test_soundex_vowels() {
+    const char* input = "aeiou";
+    const char* expected = "A000";
+    char* actual = soundex(input);
+    assert(strcmp(actual, expected) == 0);
+}
+
+void test_soundex_numbers() {
+    const char* input = "hello123";
+    const char* expected = "H400";
+    char* actual = soundex(input);
+    assert(strcmp(actual, expected) == 0);
+}
+
+void test_soundex_uppercase() {
+    const char* input = "HELLO";
+    const char* expected = "H400";
+    char* actual = soundex(input);
+    assert(strcmp(actual, expected) == 0);
+}
+
+void test_soundex_mixed_case() {
+    const char* input = "HeLlO";
+    const char* expected = "H400";
+    char* actual = soundex(input);
+    assert(strcmp(actual, expected) == 0);
+}
