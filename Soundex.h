883b8c86454daedcade2ccb8bd4822f849360368
Rakshith08URK <rajrakshith08@gmail.com>
2024-07-05 11:57:29 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,60 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
-#include <ctype.h>
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <assert.h>
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+// Soundex constants
+#define MAX_SOUNDEX_CODE_LENGTH 4
+#define SOUNDEX_CODE_SIZE (MAX_SOUNDEX_CODE_LENGTH + 1)
+
+// Soundex code mapping table
+static const char soundex_map[] = {
+    'b', 'f', 'p', 'v', // b, f, p, v
+    'c', 'g', 'j', 'k', 'q', 's', 'x', 'z', // c, g, j, k, q, s, x, z
+    'd', 't', // d, t
+    'l', // l
+    'm', 'n', // m, n
+    'r', // r
+};
+
+// Function to convert a single character to its Soundex code
+static char soundex_code(char c) {
+    c = tolower(c);
+    for (int i = 0; i < sizeof(soundex_map) / sizeof(soundex_map[0]); i++) {
+        if (strchr(soundex_map, c)) {
+            return '1' + (i / 7);
+        }
     }
+    return '0'; // default to 0 if not found
 }
 
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
+// Function to generate the Soundex code for a string
+char* soundex(const char* str) {
+    static char soundex_code[SOUNDEX_CODE_SIZE];
+    int len = strlen(str);
+    int code_idx = 0;
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
+    // Convert the first character to uppercase
+    soundex_code[code_idx++] = toupper(str[0]);
+
+    // Iterate through the rest of the string
+    for (int i = 1; i < len; i++) {
+        char c = soundex_code(tolower(str[i]));
+        if (c != '0' && c != soundex_code[code_idx - 1]) {
+            soundex_code[code_idx++] = c;
         }
     }
 
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+    // Pad with zeros if necessary
+    while (code_idx < MAX_SOUNDEX_CODE_LENGTH) {
+        soundex_code[code_idx++] = '0';
     }
 
-    soundex[4] = '\0';
+    soundex_code[code_idx] = '\0'; // null-terminate
+    return soundex_code;
 }
 
 #endif // SOUNDEX_H
