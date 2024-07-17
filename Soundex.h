aa862e49f065f00f0e73505e0da86cb610d62a83
Rakshith08URK <rajrakshith08@gmail.com>
2024-07-17 10:08:28 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -17,34 +17,45 @@ static const char soundex_map[] = {
     'r', // r
 };
 
-static char soundex_code(char c) {
+static char get_soundex_code(char c) {
     c = tolower(c);
     const char* p = strchr(soundex_map, c);
     return p ? '1' + (p - soundex_map) / 7 : '0';
 }
 
-char* soundex(const char* str) {
-    static char soundex_code[SOUNDEX_CODE_SIZE];
-    int len = strlen(str);
-    int code_idx = 0;
+static void init_soundex_code(char* soundex_code) {
+    soundex_code[0] = '\0';
+}
 
-    soundex_code[code_idx++] = toupper(str[0]);
+static void encode_first_char(const char* str, char* soundex_code) {
+    soundex_code[0] = toupper(str[0]);
+}
 
-    for (int i = 1; i < len; i++) {
+static void encode_remaining_chars(const char* str, char* soundex_code) {
+    int code_idx = 1;
+    for (int i = 1; i < strlen(str); i++) {
         char c = tolower(str[i]);
-        const char* p = strchr(soundex_map, c);
-        char code = p ? '1' + (p - soundex_map) / 7 : '0';
-
+        char code = get_soundex_code(c);
         if (code != '0' && code != soundex_code[code_idx - 1]) {
             soundex_code[code_idx++] = code;
         }
     }
+}
 
+static void pad_with_zeros(char* soundex_code) {
+    int code_idx = strlen(soundex_code);
     while (code_idx < MAX_SOUNDEX_CODE_LENGTH) {
         soundex_code[code_idx++] = '0';
     }
-
     soundex_code[code_idx] = '\0'; // null-terminate
+}
+
+char* soundex(const char* str) {
+    static char soundex_code[SOUNDEX_CODE_SIZE];
+    init_soundex_code(soundex_code);
+    encode_first_char(str, soundex_code);
+    encode_remaining_chars(str, soundex_code);
+    pad_with_zeros(soundex_code);
     return soundex_code;
 }
 
